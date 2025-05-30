5a15d14eecac4a85b65ae3637ae40016e8363a0b
FastVladimirWhite <vanthys@protonmail.com>
2025-05-30 18:39:39 +0200

    index.js

--- test api/index.js
+++ test api/index.js
@@ -1,4 +1,12 @@
+const express = require('express');  // import express
+const app = express();               // stwórz instancję aplikacji
+
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('VLPE API is running');
+});
+
 app.listen(PORT, () => {
   console.log(`VLPE API listening on port ${PORT}`);
 });
